62c21984831d55bc1eee0b4ec41392b313c79c13
丰静 <fj@shangminet.net>
2023-12-01 14:51:57 +0800

    add demo project

---
+++ GameToolDemo/GameToolDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  GameToolDemo
+//
+//  Created by smwl on 2023/12/1.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
