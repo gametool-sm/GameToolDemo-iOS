77e93642a2029f21cc685f4176f37cbed4274fad
丰静 <fj@shangminet.net>
2023-12-18 15:08:06 +0800

    更新framework

--- GameToolDemo/GameToolDemo/ViewController.m
+++ GameToolDemo/GameToolDemo/ViewController.m
@@ -8,8 +8,16 @@
 #import "ViewController.h"
 
 #import <GameToolSDK/GameToolSDK.h>
+static NSString *GTSDKStyleKey = @"GTSDKStyleKey";
+//static NSString *appKey = @"Your appKey";
+
+static NSString *appKey = @"f975a83744de431cdc300dabff7b8179";
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *appKeyTextField;
+@property (weak, nonatomic) IBOutlet UITextField *userIdTextField;
+@property (weak, nonatomic) IBOutlet UIButton *sdkInitTypeButton;
+@property (strong, nonatomic) IBOutlet UIButton *showFloatingWindowButton;
 
 @end
 
@@ -18,14 +26,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSString *appKey = @"Your appKey";
+    
     BOOL openDebug = YES;
-    [self initSDK:appKey style:GTSDKStylesDefault openDebug:openDebug];
+    GTSDKStyles style = [[NSUserDefaults standardUserDefaults] integerForKey:GTSDKStyleKey];
+    [self initSDK:appKey style:style openDebug:openDebug];
+    [self setUIWithStyle:style];
     
     
 }
 
-
+-(void)setUIWithStyle:(GTSDKStyles)style{
+    [self addLeftPadding:self.appKeyTextField];
+    [self addLeftPadding:self.userIdTextField];
+    NSString *styleStr = @"自定义";
+    self.showFloatingWindowButton.hidden = YES;
+    if(style==GTSDKStylesCustom){
+        styleStr = @"默认";
+        self.showFloatingWindowButton.hidden = NO;
+    }
+    [self.sdkInitTypeButton setTitle:styleStr forState:UIControlStateNormal];
+    self.appKeyTextField.text = [appKey isEqualToString:@"Your appKey"]?nil:appKey;
+    
+}
+-(void)addLeftPadding:(UITextField *)textField{
+    UIView *paddingLeftView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 12.0, 64.0)];
+    textField.leftViewMode = UITextFieldViewModeAlways;
+    textField.leftView = paddingLeftView;
+}
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+#pragma mark--SDK初始化
 /// 初始化SDK
 /// - Parameters:
 ///   - appKey: 登录gametool官方网站创建应用获取
@@ -38,15 +69,12 @@
 ///     YES:开启,可在管理后台调整加速器倍率
 ///     NO:不开启,加速器倍率不可调整，由加速配置文件决定
 -(void)initSDK:(NSString *)appKey style:(GTSDKStyles)style openDebug:(BOOL)openDebug{
-    __weak typeof(self)weakSelf = self;
-    [GTSDK initWithAppKey:appKey style:GTSDKStylesDefault openDebugEnvironment:YES andSuccess:^{
-        //初始化成功回调
-        NSString *userID = @"Your userID";
-        [weakSelf loginWithUserID:userID];
+    [GTSDK initWithAppKey:appKey style:GTSDKStylesDefault openDebugEnvironment:openDebug andSuccess:^{
+        NSLog(@"init success");
         
     } failure:^(NSString * _Nonnull errormsg) {
         //初始化失败回调
-        
+        NSLog(@"%@",errormsg);
     }];
 
 }
@@ -63,5 +91,39 @@
         
     }];
 }
+
+#pragma mark--click actions
+
+/// 登录
+/// - Parameter sender: <#sender description#>
+- (IBAction)loginAction:(UIButton *)sender {
+//    NSString *userID = @"Your userId";
+    NSString *userID = self.userIdTextField.text;
+    [self loginWithUserID:userID];
+}
+
+/// 切换初始化方式
+/// - Parameter sender: <#sender description#>
+- (IBAction)sdkInitTypeChangeAction:(UIButton *)sender {
+    GTSDKStyles style = [[NSUserDefaults standardUserDefaults] integerForKey:GTSDKStyleKey];
+    NSUserDefaults *userDefault = [NSUserDefaults standardUserDefaults];
+    if(style==GTSDKStylesDefault){
+        [userDefault setInteger:GTSDKStylesCustom forKey:GTSDKStyleKey];
+    }else{
+        [userDefault setInteger:GTSDKStylesDefault forKey:GTSDKStyleKey];
+    }
+    [userDefault synchronize];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        exit(0);
+    });
+    
+}
+
+/// 打开弹窗
+/// - Parameter sender: <#sender description#>
+- (IBAction)showFloatingWindowAction:(UIButton *)sender {
+    [GTSDK showFloatingWindow];
+}
+
 
 @end
